0c8e1397cb579627d27f625ef922785047bd11e5
mac <mac@macui-MacBook-Air.local>
2022-12-02 17:27:19 +0900

    이미지 엑박

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import ProductPage from "./product";
 function App() {
   return (
     <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="/images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body"></div>
+
       <Switch>
         <Route exact={true} path={"/"}>
           <MainPageComponent />
@@ -20,6 +27,8 @@ function App() {
           <UploadPage />
         </Route>
       </Switch>
+
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,39 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductPage = () => {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://701c4cb5-6989-4030-81c8-135b9278a83c.mock.pstmn.io/products/ ${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-  return <h1>상세페이지 {id} 상품</h1>;
+  if (product === null) {
+    return <h1>상품 정보 불러오는 중...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
